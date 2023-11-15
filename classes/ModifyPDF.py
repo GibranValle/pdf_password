d28ba4bc6e7c8f10553ef631930df63b861e6786
Gibran <gibricursos@gmail.com>
2023-11-15 16:27:37 -0600

    fixed missing page in preventive with CL

--- classes/ModifyPDF.py
+++ classes/ModifyPDF.py
@@ -1,9 +1,9 @@
 from PyPDF2 import PdfWriter, PdfReader
 from PyPDF2.constants import UserAccessPermissions
 import io
-from reportlab.pdfgen.canvas import Canvas
+from reportlab.pdfgen.canvas import Canvas  # type: ignore
 import os
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import letter  # type: ignore
 
 PATH = os.getcwd()
 PRINT = UserAccessPermissions.PRINT
@@ -24,38 +24,66 @@ class ModifyPDF:
         self.canvas1 = Canvas(self.packet, pagesize=letter)
         self.signed_pdf = None
 
-    def add_text(self, text, point):
-        self.canvas1.drawString(point[0], point[1], text)
+    def add_text(self, text: str, point: tuple[int, int]):
+        self.canvas1.drawString(point[0], point[1], text)  # type: ignore
 
-    def add_signature_service_order(self, point, width=120, height=120):
-        self.canvas1.drawImage(self.image, point[0], point[1], width=width, preserveAspectRatio=True, mask='auto')
+    def add_signature_service_order(self, point: tuple[int, int], width: int = 120):
+        a = self.canvas1.drawImage(  # type: ignore
+            self.image,
+            point[0],
+            point[1],
+            width=width,
+            preserveAspectRatio=True,
+            mask="auto",
+        )
 
-    def add_signature_minute(self, point, width=120, height=120):
+    def add_signature_minute(self, point: tuple[int, int], width: int = 120):
         self.canvas1.showPage()
-        self.canvas1.drawImage(self.image, point[0], point[1], width=width, preserveAspectRatio=True, mask='auto')
+        self.canvas1.drawImage(  # type: ignore
+            self.image,
+            point[0],
+            point[1],
+            width=width,
+            preserveAspectRatio=True,
+            mask="auto",
+        )
+
+    def add_signature_checklist(self, point: tuple[int, int], width: int = 120):
+        self.canvas1.showPage()
+        self.canvas1.drawImage(  # type: ignore
+            self.image,
+            point[0],
+            point[1],
+            width=width,
+            preserveAspectRatio=True,
+            mask="auto",
+        )
 
     def merge(self):
         self.canvas1.save()
         self.packet.seek(0)
         result_pdf = PdfReader(self.packet)
         for index, page in enumerate(self.template_pdf.pages):
+            print(index)
             result = result_pdf.pages[index]
             page.merge_page(result)
             self.writer.add_page(self.template_pdf.pages[index])
 
-    def generate(self, output_path: str = '', encrypted: bool = False):
+    def generate(self, output_path: str = "", encrypted: bool = False):
         output = output_path
         if encrypted:
             with open(self.password, "r") as file:
                 password = file.readline()
-            self.writer.encrypt(user_password='', owner_password=password, permissions_flag=NEEDED)
+            self.writer.encrypt(
+                user_password="", owner_password=password, permissions_flag=NEEDED  # type: ignore
+            )
 
         if len(output_path) == 0:
             if encrypted:
-                output = self.file.replace('.pdf', '_sign_lock.pdf')
+                output = self.file.replace(".pdf", "_sign_lock.pdf")
             else:
-                output = self.file.replace('.pdf', '_sign.pdf')
+                output = self.file.replace(".pdf", "_sign.pdf")
 
         with open(output, "wb") as file:
-            self.writer.write(file)
+            self.writer.write(file)  # type: ignore
         return output
--- pdf_sign.py
+++ pdf_sign.py
@@ -1,11 +1,16 @@
 import sys
-import subprocess
 import os
-from reportlab.lib.units import cm
+from reportlab.lib.units import cm  # type: ignore
 from classes.ModifyPDF import ModifyPDF
 from classes.PDFTextExtractor import PDFTextExtractor
+import subprocess
 
 PATH = os.getcwd()
+ORDER_POINTS = (2.5 * cm, 0 * cm)
+CHECKLIST_POINTS = (9 * cm, 14.75 * cm)
+MINUTE_POINTS = (9 * cm, 1.5 * cm)
+SIGNATURE_WIDTH = 3.5 * cm
+SIGNATURE_WIDTH_SMALL = 2 * cm
 
 
 class App:
@@ -17,80 +22,84 @@ class App:
         self.fields = self.extractor.fields_extract()
         self.password = password_path
         self.gen = None
+        print(self.fields)
 
-    def generate(self, output_path: str = '', encrypted: bool = False):
+    def generate(self, output_path: str = "", encrypted: bool = False):
         self.gen = ModifyPDF(self.file, self.image, self.password)
-        if 'prev' in self.fields["problem"].lower():
-            self.gen.add_signature_service_order((2.5 * cm, 0 * cm), 3.5 * cm, 1.5 * cm)
-        elif 'instal' in self.fields["problem"].lower():
-            self.gen.add_signature_service_order((2.5 * cm, 0 * cm), 3.5 * cm, 1.5 * cm)
-            self.gen.add_signature_minute((9 * cm, 1.5 * cm), 3.5 * cm, 1.5 * cm)
+        if "prev" in self.fields["problem"].lower():
+            self.gen.add_signature_service_order(ORDER_POINTS, SIGNATURE_WIDTH)  # type: ignore
+            self.gen.add_signature_checklist(CHECKLIST_POINTS, SIGNATURE_WIDTH_SMALL)  # type: ignore
+        elif "instal" in self.fields["problem"].lower():
+            self.gen.add_signature_service_order(ORDER_POINTS, SIGNATURE_WIDTH)  # type: ignore
+            self.gen.add_signature_minute(MINUTE_POINTS, SIGNATURE_WIDTH)  # type: ignore
+        elif "corr" in self.fields["problem"].lower():
+            self.gen.add_signature_service_order(ORDER_POINTS, SIGNATURE_WIDTH)  # type: ignore
         self.gen.merge()
         return self.gen.generate(output_path, encrypted)
 
     @staticmethod
     def printMenu():
-        print('------------------------------------------------')
-        print('******* PDF sign and protect application *******')
-        print('*********** FFMX by Gibran Valle ***************')
-        print('------------------------------------------------')
-        print('Please select and option:')
-        print(' 0) quit')
-        print(' 1) only sign document')
-        print(' 2) sign and protect document')
+        print("------------------------------------------------")
+        print("******* PDF sign and protect application *******")
+        print("*********** FFMX by Gibran Valle ***************")
+        print("------------------------------------------------")
+        print("Please select and option:")
+        print(" 0) quit")
+        print(" 1) only sign document")
+        print(" 2) sign and protect document")
 
     @staticmethod
-    def printEnd(encrypted=False):
-        print('File signed and protected successfully!') if encrypted else print('File signed successfully!')
-        print('------------------------------------------------')
-        print('')
+    def printEnd(encrypted: bool = False):
+        print("File signed and protected successfully!") if encrypted else print(
+            "File signed successfully!"
+        )
+        print("------------------------------------------------")
+        print("")
 
 
 def main():
-    os.system('cls')
+    os.system("cls")
     file_path = image_path = password_path = ""
     # get filepath if dragged file
     if not len(sys.argv) == 1:
         for i, arg in enumerate(sys.argv):
             if i == 1:
-                file_path = f'{arg}'
+                file_path = f"{arg}"
                 image_path = "signature.png"
                 password_path = "password.txt"
 
     else:
-        file_path = f'{PATH}\\dist\\test.pdf'
-        image_path = f'{PATH}\\dist\\signature.png'
-        password_path = f'{PATH}\\dist\\password.txt'
+        file_path = f"{PATH}\\dist\\test.pdf"
+        image_path = f"{PATH}\\dist\\signature.png"
+        password_path = f"{PATH}\\dist\\password.txt"
 
     app = App(file_path, image_path, password_path)
 
     while True:
         app.printMenu()
-        selection = input('selected: ')
-        if selection == '0':
+        selection = input("selected: ")
+        if selection == "0":
             return
-        elif selection == '1':
+        elif selection == "1":
             encrypted = False
-        elif selection == '2':
+        elif selection == "2":
             encrypted = True
         else:
-            os.system('cls')
+            os.system("cls")
             continue
-        os.system('cls')
-        print(f'selected: {selection}')
+        os.system("cls")
+        print(f"selected: {selection}")
         print(app.generate(encrypted=encrypted))
         app.printEnd(encrypted)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('keyboard close')
-    """
-except:
-    print('error')
-    call = 'pause'
-    output = subprocess.check_output(call, shell=True)
-    print(output)
-            """
+        print("keyboard close")
+    except:
+        print("error")
+        call = "pause"
+        output = subprocess.check_output(call, shell=True)
+        print(output)
